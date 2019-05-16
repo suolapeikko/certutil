3bc4cf4764551a6d7a0732a18250a2022c4d60f4
Antti Tulisalo <antti@anttis-mbp.lan>
2019-05-16 09:06:29 +0300

    Added .gitignore

---
+++ certutil/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  certutil
+//
+//  Created by Antti Tulisalo on 16/05/2019.
+//  Copyright © 2019 Antti Tulisalo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
